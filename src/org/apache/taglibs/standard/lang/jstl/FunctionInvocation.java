fda12e14cd335ba62add4e0d23bc79164eab9137
Shawn Bayern <shawn@apache.org>
2002-07-15 05:55:13 +0000

    Oops, forgot the most important piece.  (For EL function support.)

---
+++ src/org/apache/taglibs/standard/lang/jstl/FunctionInvocation.java
@@ -0,0 +1,178 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:  
+ *       "This product includes software developed by the 
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */ 
+
+package org.apache.taglibs.standard.lang.jstl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
+import java.lang.reflect.*;
+
+/**
+ *
+ * <p>Represents a function call.</p>
+ * 
+ * @author Shawn Bayern (in the style of Nathan's other classes)
+ **/
+
+public class FunctionInvocation
+  extends Expression
+{
+  //-------------------------------------
+  // Properties
+  //-------------------------------------
+  // property index
+
+  private String functionName;
+  private List argumentList;
+  public String getFunctionName() { return functionName; }
+  public void setFunctionName(String f) { functionName = f; }
+  public List getArgumentList() { return argumentList; }
+  public void setArgumentList(List l) { argumentList = l; }
+
+  //-------------------------------------
+  /**
+   * Constructor
+   **/
+  public FunctionInvocation (String functionName, List argumentList)
+  {
+    this.functionName = functionName;
+    this.argumentList = argumentList;
+  }
+
+  //-------------------------------------
+  // Expression methods
+  //-------------------------------------
+  /**
+   * Returns the expression in the expression language syntax
+   **/
+  public String getExpressionString ()
+  {
+    StringBuffer b = new StringBuffer();
+    b.append(functionName);
+    b.append("(");
+    Iterator i = argumentList.iterator();
+    while (i.hasNext()) {
+      b.append(((Expression) i.next()).getExpressionString());
+      if (i.hasNext())
+        b.append(", ");
+    }
+    b.append(")");
+    return b.toString();
+  }
+
+
+  //-------------------------------------
+  /**
+   *
+   * Evaluates by looking up the name in the VariableResolver
+   **/
+  public Object evaluate (Object pContext,
+                          VariableResolver pResolver,
+			  Map functions,
+			  String defaultPrefix,
+                          Logger pLogger)
+    throws ELException
+  {
+
+    // if the Map is null, then the function is invalid
+    if (functions == null)
+      pLogger.logError(Constants.UNKNOWN_FUNCTION, functionName);
+
+    // normalize function name against default prefix
+    String functionName = this.functionName;
+    if (functionName.indexOf(":") == -1) {
+      if (defaultPrefix == null)
+        pLogger.logError(Constants.UNKNOWN_FUNCTION, functionName);
+      functionName = defaultPrefix + ":" + functionName;
+    }
+
+    // ensure that the number of arguments matches the number of parameters
+    Method target = (Method) functions.get(functionName);
+    Class[] params = target.getParameterTypes();
+    if (params.length != argumentList.size())
+      pLogger.logError(Constants.INAPPROPRIATE_FUNCTION_ARG_COUNT,
+		       new Integer(params.length),
+		       new Integer(argumentList.size()));
+
+    // now, walk through each parameter, evaluating and casting its argument
+    Object[] arguments = new Object[argumentList.size()];
+    for (int i = 0; i < params.length; i++) {
+      // evaluate
+      arguments[i] = ((Expression) argumentList.get(i)).evaluate(pContext,
+								 pResolver,
+								 functions,
+								 defaultPrefix,
+								 pLogger);
+      // coerce
+      arguments[i] = Coercions.coerce(arguments[i], params[i], pLogger);
+    }
+
+    // finally, invoke the target method, which we know to be static
+    try {
+      return (target.invoke(null, arguments));
+    } catch (InvocationTargetException ex) {
+      pLogger.logError(Constants.FUNCTION_INVOCATION_ERROR,
+			ex.getTargetException(),
+			functionName);
+      return null;
+    } catch (Exception ex) {
+      pLogger.logError(Constants.FUNCTION_INVOCATION_ERROR, ex, functionName);
+      return null;
+    }
+  }
+
+  //-------------------------------------
+}
