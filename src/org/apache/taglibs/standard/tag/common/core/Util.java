c3d8288f05c51de4e45eda94333843d6943c1c96
Pierre Delisle <pierred@apache.org>
2003-03-31 15:53:51 +0000

    fix to the parsing of content-type attribute.

--- src/org/apache/taglibs/standard/tag/common/core/Util.java
+++ src/org/apache/taglibs/standard/tag/common/core/Util.java
@@ -175,16 +175,21 @@ public class Util {
 	int end;
         int index = input.toUpperCase().indexOf(name.toUpperCase());
         if (index == -1) return null;
-        index += 8; // charset=
-        if (input.charAt(index) == '"') {
+        index = index + name.length(); // positioned after the attribute name
+        index = input.indexOf('=', index); // positioned at the '='
+        if (index == -1) return null;
+        index += 1; // positioned after the '='
+        input = input.substring(index).trim();
+        
+        if (input.charAt(0) == '"') {
             // attribute value is a quoted string
-            begin = index + 1;
+            begin = 1;
             end = input.indexOf('"', begin);
             if (end == -1) return null;
         } else {
-            begin = index;
-            end = input.indexOf(';', begin);
-            if (end == -1) end = input.indexOf(' ', begin);
+            begin = 0;
+            end = input.indexOf(';');
+            if (end == -1) end = input.indexOf(' ');
             if (end == -1) end = input.length();
         }
         return input.substring(begin, end).trim();
