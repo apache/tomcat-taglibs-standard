112a58f8e893ecdb944ce2b288410dfd7990c6a3
Pierre Delisle <pierred@apache.org>
2005-02-17 00:59:55 +0000

    Patch from Dhiru Pandey for bug #32755
    EmptyOperator does not evaluate instances of Collection
    
    Description:
    
    org.apache.taglibs.standard.lang.jstl.EmptyOperator is evaluating only List but
    not Collection instances. Any Collection different from List returns not empty.
    
    The fix is simple and proposed by the submitter of the bug Edgar Poce (edgarpoce@gmail.com)

--- src/org/apache/taglibs/standard/lang/jstl/EmptyOperator.java
+++ src/org/apache/taglibs/standard/lang/jstl/EmptyOperator.java
@@ -14,94 +14,187 @@
  * limitations under the License.
  */ 
 
+
+
 package org.apache.taglibs.standard.lang.jstl;
 
+
+
 import java.lang.reflect.Array;
-import java.util.List;
+
+import java.util.Collection;
+
 import java.util.Map;
 
+
+
 /**
+
  *
+
  * <p>The implementation of the empty operator
+
  * 
+
  * @author Nathan Abramson - Art Technology Group
+
  * @version $Change: 181177 $$DateTime: 2001/06/26 08:45:09 $$Author$
+
  **/
 
+
+
 public class EmptyOperator
+
   extends UnaryOperator
+
 {
+
   //-------------------------------------
+
   // Singleton
+
   //-------------------------------------
+
+
 
   public static final EmptyOperator SINGLETON =
+
     new EmptyOperator ();
 
+
+
   //-------------------------------------
+
   /**
+
    *
+
    * Constructor
+
    **/
+
   public EmptyOperator ()
+
   {
+
   }
 
+
+
   //-------------------------------------
+
   // Expression methods
-  //-------------------------------------
-  /**
-   *
-   * Returns the symbol representing the operator
-   **/
-  public String getOperatorSymbol ()
-  {
-    return "empty";
-  }
 
   //-------------------------------------
+
   /**
+
    *
-   * Applies the operator to the given value
+
+   * Returns the symbol representing the operator
+
    **/
-  public Object apply (Object pValue,
-		       Object pContext,
-		       Logger pLogger)
-    throws ELException
+
+  public String getOperatorSymbol ()
+
   {
+
+    return "empty";
+
+  }
+
+
+
+  //-------------------------------------
+
+  /**
+
+   *
+
+   * Applies the operator to the given value
+
+   **/
+
+  public Object apply (Object pValue,
+
+		       Object pContext,
+
+		       Logger pLogger)
+
+    throws ELException
+
+  {
+
     // See if the value is null
+
     if (pValue == null) {
+
       return PrimitiveObjects.getBoolean (true);
+
     }
+
+
 
     // See if the value is a zero-length String
+
     else if ("".equals (pValue)) {
+
       return PrimitiveObjects.getBoolean (true);
+
     }
+
+
 
     // See if the value is a zero-length array
+
     else if (pValue.getClass ().isArray () &&
+
 	     Array.getLength (pValue) == 0) {
+
       return PrimitiveObjects.getBoolean (true);
+
     }
 
-    // See if the value is an empty List
-    else if (pValue instanceof List &&
-	     ((List) pValue).isEmpty ()) {
+
+
+    // See if the value is an empty Collection
+
+    else if (pValue instanceof Collection &&
+
+	     ((Collection) pValue).isEmpty ()) {
+
       return PrimitiveObjects.getBoolean (true);
+
     }
+
+
 
     // See if the value is an empty Map
+
     else if (pValue instanceof Map &&
+
 	     ((Map) pValue).isEmpty ()) {
+
       return PrimitiveObjects.getBoolean (true);
+
     }
+
+
 
     // Otherwise, not empty
+
     else {
+
       return PrimitiveObjects.getBoolean (false);
+
     }
+
   }
 
+
+
   //-------------------------------------
+
 }
+
