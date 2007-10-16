ad8be0f255f5167102ad318159773f4587ebd282
Henri Yandell <bayard@apache.org>
2007-10-16 05:40:16 +0000

    Applying the fix and the tests from #32311 - LRUMap used as a caching system to improve on performance

--- src/org/apache/taglibs/standard/tag/common/fmt/FormatDateSupport.java
+++ src/org/apache/taglibs/standard/tag/common/fmt/FormatDateSupport.java
@@ -22,6 +22,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.Collections;
+import java.util.Map;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
@@ -30,6 +32,8 @@ import javax.servlet.jsp.tagext.TagSupport;
 
 import org.apache.taglibs.standard.resources.Resources;
 import org.apache.taglibs.standard.tag.common.core.Util;
+
+import org.apache.taglibs.standard.extra.commons.collections.map.LRUMap;
 
 /**
  * Support for tag handlers for &lt;formatDate&gt;, the date and time
@@ -43,9 +47,23 @@ public abstract class FormatDateSupport extends TagSupport {
     //*********************************************************************
     // Private constants
 
+	/**
+	 * Name of configuration setting for maximum number of entries in the
+	 * cached dateformat map
+	 */
+	private static final String DATE_CACHE_PARAM = 
+		"org.apache.taglibs.standard.tag.common.fmt.dateFormatCacheSize";
+
     private static final String DATE = "date";
     private static final String TIME = "time";
     private static final String DATETIME = "both";
+
+    private static Map dateFormatCache = null;
+
+	/**
+	 * Default maximum  cache size
+	 */
+	private static final int MAX_SIZE = 100;
 
 
     //*********************************************************************
@@ -120,16 +138,7 @@ public abstract class FormatDateSupport extends TagSupport {
 	    DateFormat.getAvailableLocales());
 
 	if (locale != null) {
-	    DateFormat formatter = createFormatter(locale);
-
-	    // Apply pattern, if present
-	    if (pattern != null) {
-		try {
-		    ((SimpleDateFormat) formatter).applyPattern(pattern);
-		} catch (ClassCastException cce) {
-		    formatter = new SimpleDateFormat(pattern, locale);
-		}
-	    }
+	    DateFormat formatter = createFormatter(locale, pattern);
 
 	    // Set time zone
 	    TimeZone tz = null;
@@ -180,22 +189,75 @@ public abstract class FormatDateSupport extends TagSupport {
     //*********************************************************************
     // Private utility methods
 
-    private DateFormat createFormatter(Locale loc) throws JspException {
+    private DateFormat createFormatter(Locale loc, String pattern) throws JspException {
 	DateFormat formatter = null;
 
+	// lazy initialization of cache
+	if (dateFormatCache == null) {
+		String value = pageContext.getServletContext().getInitParameter(DATE_CACHE_PARAM);
+		if (value != null) {
+			dateFormatCache = Collections.synchronizedMap(new LRUMap(Integer.parseInt(value)));
+		} else {
+			dateFormatCache = Collections.synchronizedMap(new LRUMap(MAX_SIZE));
+		}
+	}
+
+	// Apply pattern, if present
+	if (pattern != null) {
+		if ((type == null) || DATE.equalsIgnoreCase(type)) {
+		    String key = DATE + pattern + loc;
+            formatter = (DateFormat) dateFormatCache.get(key);
+            if(formatter == null) {
+                formatter = new SimpleDateFormat(pattern, loc);
+                dateFormatCache.put(key, formatter);
+            }
+		} else if (TIME.equalsIgnoreCase(type)) {
+		    String key = TIME + pattern + loc;
+            formatter = (DateFormat) dateFormatCache.get(key);
+            if(formatter == null) {
+                formatter = new SimpleDateFormat(pattern, loc);
+                dateFormatCache.put(key, formatter);
+            }
+		} else if (DATETIME.equalsIgnoreCase(type)) {
+		    String key = DATETIME + pattern + loc;
+            formatter = (DateFormat) dateFormatCache.get(key);
+            if(formatter == null) {
+                formatter = new SimpleDateFormat(pattern, loc);
+                dateFormatCache.put(key, formatter);
+            }
+		} else {
+		    throw new JspException(
+  	                  Resources.getMessage("FORMAT_DATE_INVALID_TYPE", 
+						 type));
+		}
+        return formatter;
+	}
+
 	if ((type == null) || DATE.equalsIgnoreCase(type)) {
-	    formatter = DateFormat.getDateInstance(
-	        Util.getStyle(dateStyle, "FORMAT_DATE_INVALID_DATE_STYLE"),
-		loc);
+		int style = Util.getStyle(dateStyle, "FORMAT_DATE_INVALID_DATE_STYLE");
+		String key = DATE + style + loc;
+		formatter = (DateFormat) dateFormatCache.get(key);
+		if(formatter == null) {
+			formatter = DateFormat.getDateInstance(style, loc);
+			dateFormatCache.put(key, formatter);
+		}
 	} else if (TIME.equalsIgnoreCase(type)) {
-	    formatter = DateFormat.getTimeInstance(
-	        Util.getStyle(timeStyle, "FORMAT_DATE_INVALID_TIME_STYLE"),
-		loc);
+		int style = Util.getStyle(timeStyle, "FORMAT_DATE_INVALID_TIME_STYLE");
+		String key = TIME + style + loc;
+		formatter = (DateFormat) dateFormatCache.get(key);
+		if(formatter == null) {
+			formatter = DateFormat.getTimeInstance(style, loc);
+			dateFormatCache.put(key, formatter);
+		}
 	} else if (DATETIME.equalsIgnoreCase(type)) {
-	    formatter = DateFormat.getDateTimeInstance(
-	        Util.getStyle(dateStyle, "FORMAT_DATE_INVALID_DATE_STYLE"),
-		Util.getStyle(timeStyle, "FORMAT_DATE_INVALID_TIME_STYLE"),
-		loc);
+		int style1 = Util.getStyle(dateStyle, "FORMAT_DATE_INVALID_DATE_STYLE");
+		int style2 = Util.getStyle(timeStyle, "FORMAT_DATE_INVALID_TIME_STYLE");
+		String key = DATETIME + style1 + loc + style2;
+		formatter = (DateFormat) dateFormatCache.get(key);
+		if(formatter == null) {
+			formatter = DateFormat.getDateTimeInstance(style1, style2, loc);
+			dateFormatCache.put(key, formatter);
+		}
 	} else {
 	    throw new JspException(
                     Resources.getMessage("FORMAT_DATE_INVALID_TYPE", 
