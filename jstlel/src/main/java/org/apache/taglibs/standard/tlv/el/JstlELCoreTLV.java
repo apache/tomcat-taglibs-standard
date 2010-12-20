977d1977f2b932aaf3b3c8180a9738f7e7ffea5a
Jeremy Boynes <jboynes@apache.org>
2010-12-20 01:09:29 +0000

    Fix #50414 by moving EL TLVs to sub-package

--- jstlel/src/main/java/org/apache/taglibs/standard/tlv/el/JstlELCoreTLV.java
+++ jstlel/src/main/java/org/apache/taglibs/standard/tlv/el/JstlELCoreTLV.java
@@ -14,7 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.taglibs.standard.tlv;
+package org.apache.taglibs.standard.tlv.el;
+
+import org.apache.taglibs.standard.tlv.JstlCoreTLV;
 
 /**
  */
--- jstlel/src/main/java/org/apache/taglibs/standard/tlv/el/ValidationUtil.java
+++ jstlel/src/main/java/org/apache/taglibs/standard/tlv/el/ValidationUtil.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.taglibs.standard.tlv;
+package org.apache.taglibs.standard.tlv.el;
 
 import javax.servlet.jsp.JspException;
 
